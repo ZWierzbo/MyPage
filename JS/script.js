f8499fd326186986cdcde57a5ea84a6af7cecc22
Zuzanna Wierzbowska <zuzanna.wierzbowska@gmail.com>
2020-06-25 15:30:27 +0100

    First commit

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+//Motyw
+let button = document.querySelector(".button")
+let mode = document.querySelector("body");
+
+button.addEventListener("click", () => {
+    if (button.innerText === "Włącz ciemny motyw") {
+        button.innerText = "Wyłącz ciemny motyw";
+        mode.classList.toggle("kolor");
+    } else {
+        button.innerText = "Włącz ciemny motyw";
+        mode.classList.toggle("kolor");
+    }
+});
+
+//Usuniecie zdjecia
+let button1 = document.querySelector(".przycisk");
+let zdjecie = document.querySelector(".zdjecie");
+
+button1.addEventListener("click", () => {
+    if (button1.innerText === "Ukryj zdjęcie") {
+        zdjecie.style.visibility = "hidden";
+        button1.innerText = "Pokaz zdjecie";
+    } else {
+        zdjecie.style.visibility = "visible";
+        button1.innerText = "Ukryj zdjęcie"
+    }
+});
