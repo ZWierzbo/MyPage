08f1d2095c8e5a1fa5e295c63e91d6205910a3db
Zuzanna Wierzbowska <zuzanna.wierzbowska@gmail.com>
2020-06-26 16:18:41 +0100

    BEM implementation 3

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,27 @@
-//Motyw
-let button = document.querySelector(".button")
-let mode = document.querySelector("body");
+//Change background to a dark mode
+let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton")
+let modeChange = document.querySelector("body");
 
-button.addEventListener("click", () => {
-    if (button.innerText === "Włącz ciemny motyw") {
-        button.innerText = "Wyłącz ciemny motyw";
-        mode.classList.toggle("kolor");
+changeBackgroundButton.addEventListener("click", () => {
+    if (changeBackgroundButton.innerText === "Włącz ciemny motyw") {
+        changeBackgroundButton.innerText = "Wyłącz ciemny motyw";
+        modeChange.classList.toggle("darkMode");
     } else {
-        button.innerText = "Włącz ciemny motyw";
-        mode.classList.toggle("kolor");
+        changeBackgroundButton.innerText = "Włącz ciemny motyw";
+        modeChange.classList.toggle("darkMode");
     }
 });
 
-//Usuniecie zdjecia
-let button1 = document.querySelector(".przycisk");
-let zdjecie = document.querySelector(".js-picture");
+//Hide the photo
+let hideImgButton = document.querySelector(".js-removeImageButton");
+let image = document.querySelector(".js-picture");
 
-button1.addEventListener("click", () => {
-    if (button1.innerText === "Ukryj zdjęcie") {
-        zdjecie.style.visibility = "hidden";
-        button1.innerText = "Pokaz zdjecie";
+hideImgButton.addEventListener("click", () => {
+    if (hideImgButton.innerText === "Ukryj zdjęcie") {
+        image.style.visibility = "hidden";
+        hideImgButton.innerText = "Pokaz zdjęcie";
     } else {
-        zdjecie.style.visibility = "visible";
-        button1.innerText = "Ukryj zdjęcie"
+        image.style.visibility = "visible";
+        hideImgButton.innerText = "Ukryj zdjęcie"
     }
 });
