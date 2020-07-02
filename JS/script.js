20a6086e599ce7f0d720394a2a71cc7f34afc12d
Zuzanna Wierzbowska <zuzanna.wierzbowska@gmail.com>
2020-07-02 14:11:11 +0100

    js code edit

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,48 @@
-//Change background to a dark mode
-let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton")
-let modeChange = document.querySelector("body");
-
-changeBackgroundButton.addEventListener("click", () => {
-    if (changeBackgroundButton.innerText === "Włącz ciemny motyw") {
-        changeBackgroundButton.innerText = "Wyłącz ciemny motyw";
-        modeChange.classList.toggle("darkMode");
-    } else {
-        changeBackgroundButton.innerText = "Włącz ciemny motyw";
-        modeChange.classList.toggle("darkMode");
+{
+    const welcome = () => {
+        console.log("Hello everyone! Hope you're having a wonderful day!")
     }
-});
 
-//Hide the photo
-let hideImgButton = document.querySelector(".js-removeImageButton");
-let image = document.querySelector(".js-picture");
+    welcome();
 
-hideImgButton.addEventListener("click", () => {
-    if (hideImgButton.innerText === "Ukryj zdjęcie") {
-        image.style.visibility = "hidden";
-        hideImgButton.innerText = "Pokaz zdjęcie";
-    } else {
-        image.style.visibility = "visible";
-        hideImgButton.innerText = "Ukryj zdjęcie"
-    }
-});
+    //Hide the photo
+
+    const onHideImageClick = () => {
+        const image = document.querySelector(".js-picture")
+
+        if (hideImgButton.innerText === "Ukryj zdjęcie") {
+            image.style.visibility = "hidden";
+            hideImgButton.innerText = "Pokaz zdjęcie";
+        } else {
+            image.style.visibility = "visible";
+            hideImgButton.innerText = "Ukryj zdjęcie"
+        }
+    };
+
+    const hideImgButton = document.querySelector(".js-removeImageButton")
+    hideImgButton.addEventListener("click", onHideImageClick);
+
+
+    //Change background to a dark mode
+    
+    const onChangeBackgroundClick = () => {
+        const modeChange = document.querySelector("body");
+
+        if (changeBackgroundButton.innerText === "Włącz ciemny motyw") {
+            changeBackgroundButton.innerText = "Wyłącz ciemny motyw";
+            modeChange.classList.toggle("darkMode");
+        } else {
+            changeBackgroundButton.innerText = "Włącz ciemny motyw";
+            modeChange.classList.toggle("darkMode");
+        }
+    };
+
+    const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton")
+    changeBackgroundButton.addEventListener("click", onChangeBackgroundClick);
+
+}
+
+
+
+
+
